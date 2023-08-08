3a3b6b92a48373b66ae2b95b0d3ddae364c54bba
突突兔 <37355028+tututu-patch@users.noreply.github.com>
2023-08-08 15:14:39 +0800

    test
    test

--- etl/get-stars.sh
+++ etl/get-stars.sh
@@ -9,7 +9,8 @@ URL="https://api.github.com/users/$USERNAME/starred?per_page=100"
 COUNTER=1
 while [ "$URL" ]; do
   RES=$(curl -i -Ss -H "Accept: application/vnd.github.v3.star+json" -H "Authorization: token $GITHUB_TOKEN" "$URL")
-  
+  echo "$RES"
+  echo "$URL"
   # via https://michaelheap.com/follow-github-link-header-bash/
   HEADERS=$(echo "$RES" | sed '/^\r$/q')
   URL=$(echo "$HEADERS" | sed -n -E 's/link:.*<(.*)>; rel="next".*/\1/p')
